04523176a528a59c1c595b74914ce0dc5a356ebc
Paul <paul.lugagnedelpon@gmail.com>
2014-07-21 09:25:51 +0800

    Refactoring debit_credit service object, prepare for additional transactions.

--- app/models/debit_credit.rb
+++ app/models/debit_credit.rb
@@ -1,14 +1,12 @@
 class DebitCredit
 
   def debit_wager(params)
-    head :not_found unless user = User.find_by( id: params[:user_id] )
     
-    oldCoins = user.coins
-    wager = params[:wager]
-    newCoins = oldCoins - wager
+    get_user( params[:user_id] )
+    set_new_coins( -params[:wager] )
     
     sql = %{
-      UPDATE "users" SET "coins" = #{newCoins}, "updated_at" = now() at time zone 'utc'
+      UPDATE "users" SET "coins" = #{@newCoins}, "updated_at" = now() at time zone 'utc'
       WHERE "users"."id" = '#{params[:user_id]}'
     }.squish
     
@@ -16,24 +14,16 @@ class DebitCredit
   end
 
   def record_debit_transaction(params, betId)
-
-    head :not_found unless user = User.find_by( id: params[:user_id] )
     
-    oldCoins = user.coins
-    wager = params[:wager]
-    newCoins = oldCoins - wager
-
-    uuid = ActiveRecord::Base.connection
-      .raw_connection
-      .exec("select uuid_generate_v4();")
-      .field_values("uuid_generate_v4")
-      .first
+    get_user( params[:user_id] )
+    set_new_coins( -params[:wager] )
+    get_uuid
 
     sql = %{
       INSERT INTO coin_transactions 
         ("transaction_type", "bet_reference", "before_value", "after_value", "id", "user_id", "created_at", "updated_at") 
       VALUES
-        ('place_bet', '#{betId}', #{oldCoins}, #{newCoins}, '#{uuid}', '#{params[:user_id]}', now() at time zone 'utc', now() at time zone 'utc')
+        ('place_bet', '#{betId}', #{@oldCoins}, #{@newCoins}, '#{@uuid}', '#{params[:user_id]}', now() at time zone 'utc', now() at time zone 'utc')
     }.squish
     ActiveRecord::Base.connection.execute(sql).to_a
   end
@@ -41,6 +31,26 @@ class DebitCredit
 
   def record_init_transaction(params)
     # TODO: RECORD TRANSACTION WHEN INIT 1000 COINS AT SIGN UP
+  end
+
+
+  protected
+
+  def set_new_coins(credit)
+    @oldCoins = @user.coins
+    @newCoins = @oldCoins + credit
+  end
+
+  def get_user(userId)
+    head :not_found unless @user = User.find_by( id: userId )
+  end
+
+  def get_uuid
+    @uuid = ActiveRecord::Base.connection
+      .raw_connection
+      .exec("select uuid_generate_v4();")
+      .field_values("uuid_generate_v4")
+      .first
   end
 
 end
